75e542886d3400f4cc34f57d13c9e33f3e2846ef
Emiri Maegawa <101238309+mgwem@users.noreply.github.com>
2022-07-21 07:34:25 +0000

    [Add]group作成　groups/index,newページ

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,7 +1,38 @@
 class GroupsController < ApplicationController
+  before_action :authenticate_user!
+
   def index
+    @groups = Group.all
+    @book = Book.new
   end
 
   def show
   end
+
+  def new
+    @group = Group.new
+  end
+
+  def create
+    @group = Group.new(group_params)
+    @group.owner_id = current_user.id
+    if @group.save
+      redirect_to groups_path, notice: "グループを作成しました"
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  private
+
+  def group_params
+    params.require(:group).permit(:name, :introduction, :owner_id, :group_image)
+  end
+
 end
--- app/models/group.rb
+++ app/models/group.rb
@@ -1,9 +1,11 @@
 class Group < ApplicationRecord
   has_many :group_users, dependent: :destroy
-  
+
+  validates :name, presence: true
+
   has_one_attached :group_image
-  
+
   def get_group_image
-    (profile_image.attached?) ? group_image : 'no_image.jpg'
+    (group_image.attached?) ? group_image : 'no_image.jpg'
   end
 end
